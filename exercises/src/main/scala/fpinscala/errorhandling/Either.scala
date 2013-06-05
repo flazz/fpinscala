4994a5434e312c1f95b3ac14c67e9a4a3d11729c
Francesco Lazzarino <flazzarino@gmail.com>
2013-06-04 23:27:03 -0500

    done with error handling

--- exercises/src/main/scala/fpinscala/errorhandling/Either.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Either.scala
@@ -1,11 +1,20 @@
 package fpinscala.errorhandling
 
 sealed trait Either[+E,+A] {
- def map[B](f: A => B): Either[E, B] = sys.error("todo")
+ def map[B](f: A => B): Either[E, B] = this match {
+   case l@Left(_e) => l
+   case Right(a) => (f andThen Right.apply)(a)
+ }
 
- def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = sys.error("todo")
+ def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+   case l@Left(_e) => l
+   case r@Right(a) => f(a)
+ }
 
- def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = sys.error("todo")
+ def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+   case Left(_e) => b
+   case r@Right(a) => r
+ }
 
  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = sys.error("todo")
 }
@@ -13,13 +22,13 @@ case class Left[+E](get: E) extends Either[E,Nothing]
 case class Right[+A](get: A) extends Either[Nothing,A]
 
 object Either {
-  def mean(xs: IndexedSeq[Double]): Either[String, Double] = 
-    if (xs.isEmpty) 
-      Left("mean of empty list!") 
-    else 
+  def mean(xs: IndexedSeq[Double]): Either[String, Double] =
+    if (xs.isEmpty)
+      Left("mean of empty list!")
+    else
       Right(xs.sum / xs.length)
 
-  def safeDiv(x: Double, y: Double): Either[Exception, Double] = 
+  def safeDiv(x: Double, y: Double): Either[Exception, Double] =
     try {
       Right(x / y)
     } catch {
--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -5,15 +5,15 @@ sealed trait Option[+A] {
     case None => None
     case Some(a) => Some(f(a))
   }
-  
+
   def getOrElse[B>:A](default: => B): B = this match {
     case None => default
     case Some(a) => a
   }
-  
-  def flatMap[B](f: A => Option[B]): Option[B] = 
+
+  def flatMap[B](f: A => Option[B]): Option[B] =
     map(f) getOrElse None
-  
+
   /*
   Of course, we can also implement `flatMap` with explicit pattern matching.
   */
@@ -21,18 +21,18 @@ sealed trait Option[+A] {
     case None => None
     case Some(a) => f(a)
   }
-  
-  def orElse[B>:A](ob: => Option[B]): Option[B] = 
+
+  def orElse[B>:A](ob: => Option[B]): Option[B] =
     this map (Some(_)) getOrElse ob
-  
+
   /*
-  Again, we can implement this with explicit pattern matching. 
+  Again, we can implement this with explicit pattern matching.
   */
   def orElse_1[B>:A](ob: => Option[B]): Option[B] = this match {
-    case None => ob 
+    case None => ob
     case _ => this
   }
-  
+
   def filter(f: A => Boolean): Option[A] = this match {
     case Some(a) if f(a) => this
     case _ => None
@@ -61,7 +61,7 @@ object Option {
     else Some(xs.sum / xs.length)
 
   import java.util.regex._
-  
+
   def pattern(s: String): Option[Pattern] =
     try {
       Some(Pattern.compile(s))
@@ -69,15 +69,15 @@ object Option {
       case e: PatternSyntaxException => None
     }
 
-  def mkMatcher(pat: String): Option[String => Boolean] = 
+  def mkMatcher(pat: String): Option[String => Boolean] =
     pattern(pat) map (p => (s: String) => p.matcher(s).matches) // The details of this API don't matter too much, but `p.matcher(s).matches` will check if the string `s` matches the pattern `p`.
 
-  def mkMatcher_1(pat: String): Option[String => Boolean] = 
+  def mkMatcher_1(pat: String): Option[String => Boolean] =
     for {
       p <- pattern(pat)
     } yield ((s: String) => p.matcher(s).matches)
-  
-  def doesMatch(pat: String, s: String): Option[Boolean] = 
+
+  def doesMatch(pat: String, s: String): Option[Boolean] =
     for {
       p <- mkMatcher_1(pat)
     } yield p(s)
@@ -89,16 +89,44 @@ object Option {
     } yield f(s) && g(s)
 
   def bothMatch_1(pat: String, pat2: String, s: String): Option[Boolean] =
-    mkMatcher(pat) flatMap (f => 
-    mkMatcher(pat2) map     (g => 
+    mkMatcher(pat) flatMap (f =>
+    mkMatcher(pat2) map     (g =>
     f(s) && g(s)))
-  def variance(xs: Seq[Double]): Option[Double] = sys.error("todo")
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = sys.error("todo")
+  def variance(xs: Seq[Double]): Option[Double] =
+    mean(xs) flatMap { m =>
+      val squaredDeviations = xs map { x => math.pow(x - m, 2) }
+      mean(squaredDeviations)
+    }
 
-  def bothMatch_2(pat1: String, pat2: String, s: String): Option[Boolean] = sys.error("todo")
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    a flatMap { av =>
+      b map { bv =>
+        f(av, bv)
+      }
+    }
 
-  def sequence[A](a: List[Option[A]]): Option[List[A]] = sys.error("todo")
+  def bothMatch_2(pat1: String, pat2: String, s: String): Option[Boolean] =
+    map2(mkMatcher(pat1), mkMatcher(pat2)){ (m1, m2) => m1(s) && m2(s) }
 
-  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = sys.error("todo")
+  def sequence[A](a: List[Option[A]]): Option[List[A]] =
+    a match {
+      case Nil => Some(Nil)
+      case h :: t =>
+        h flatMap { hh =>
+          sequence(t) map { tt => hh :: tt }
+        }
+    }
+
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] =
+    a match {
+      case Nil => Some(Nil)
+      case h :: t =>
+        f(h) flatMap { hh =>
+          traverse(t)(f) map { tt => hh :: tt }
+        }
+    }
+
+  def sequence_2[A](a: List[Option[A]]): Option[List[A]] = traverse(a)(identity)
+
 }
